720417c0873a4284c22aac6d26dc0dd277eabd39
Thompson Plyler <nealok.g@gmail.com>
2018-11-08 23:14:18 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,9 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {
-		:railroads => {:pieces => 4, :names => {}
+		:railroads => {  	:pieces => 4,
+											:names => {},
+											:rent_in_dollars => {}
 		}
 	}
 end
